9e7ba5630b7dbafe7d135d0c9ef4784171b9f874
DN0000 <DN0000@MDJBHKR3>
2025-03-09 14:15:56 +0530

    My First SIP calculator using Java+Spring boot

---
+++ SIPCalculator/src/main/java/com/sip/pojo/Parameters.java
@@ -0,0 +1,29 @@
+package com.sip.pojo;
+
+public class Parameters {
+	private double principal;
+	
+	private double interest;
+	
+	private int noOfYears;
+	
+	public double getPrincipal() {
+		return principal;
+	}
+	public void setPrincipal(double principal) {
+		this.principal = principal;
+	}
+	public double getInterest() {
+		return interest;
+	}
+	public void setInterest(double interest) {
+		this.interest = interest;
+	}
+	public int getNoOfYears() {
+		return noOfYears;
+	}
+	public void setNoOfYears(int noOfYears) {
+		this.noOfYears = noOfYears;
+	}
+	
+}
